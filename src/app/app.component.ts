2d0daf5b5522c25c6f3cdaa4ff4ac9cdd09608eb
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2020-10-03 13:14:06 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root></app-root>
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'sda-app';
+  title = 'Awesome sda app!!!';
+  description = 'This is our first app in ';
+  framework = 'angular';
 }
