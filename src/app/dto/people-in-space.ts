8a933e50ce696f2bd6cf57bf46cd1b346a7c4d71
Marcin <marcinzielinski1983@gmail.com>
2020-10-08 20:49:55 +0100

    still unsolved problem

--- src/app/dto/people-in-space.ts
+++ src/app/dto/people-in-space.ts
@@ -4,18 +4,32 @@
  {"craft": "ISS", "name": "Anatoly Ivanishin"}, {"craft": "ISS", "name": "Ivan Vagner"}],
   "message": "success"}
  */
-export interface PeopleInSpace{
-number: number;
-people: [
+// export interface PeopleInSpace{
+// number: number;
+// people: [
 
-{ craft: String, name: String}[],
-{ craft: String, name: String}[],
-{ craft: String, name: String}[]
+// { craft: String, name: String}[],
+// { craft: String, name: String}[],
+// { craft: String, name: String}[]
 
-];
-
-
+// ];
 
 
-message: String;
+
+
+// message: String;
+// }
+
+export interface PeopleInS {
+    craft: string;
+    name: string;
 }
+
+
+
+export interface PeopleInSpace {
+    number: number;
+    people: PeopleInS[];
+    message: string;
+}
+
